cdd45cd96350f04bc853f78a7d9ed21c0f7be7b4
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-22 16:30:44 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,52 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        # Return all messages ordered by created_at in ascending order
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_serialized = [message.to_dict() for message in messages]
+        return jsonify(messages_serialized)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    elif request.method == 'POST':
+        # Create a new message with body and username from params
+        data = request.get_json()
+        body = data.get('body')
+        username = data.get('username')
+
+        if not body or not username:
+            return jsonify({'message': 'Missing body or username in request'}), 400
+
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def message_by_id(id):
+    message = Message.query.get(id)
+
+    if not message:
+        return jsonify({'message': 'Message not found'}), 404
+
+    if request.method == 'PATCH':
+        # Update the body of the message using params
+        data = request.get_json()
+        new_body = data.get('body')
+
+        if not new_body:
+            return jsonify({'message': 'Missing body in request'}), 400
+
+        message.body = new_body
+        db.session.commit()
+        return jsonify(message.to_dict())
+
+    elif request.method == 'DELETE':
+        # Delete the message from the database
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({'message': 'Message deleted'})
 
 if __name__ == '__main__':
     app.run(port=5555)
